6fa8b47295904a145804dbf83b8116b2cb4b200c
Nick Alcorso <nalcorso@gmail.com>
2022-05-09 10:51:04 +1000

    Add simple utility finction to check for Delegate types

---
+++ src/fluid.inject/Extensions/TypeExtensions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2022, Nick Alcorso <nalcorso@gmail.com>
+
+namespace Fluid.Inject.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool IsDelegate(this Type type)
+    {
+        return type.IsSubclassOf(typeof(Delegate)) || (type == typeof(Delegate));
+    }
+}
