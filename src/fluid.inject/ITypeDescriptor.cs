088d813d7fc52625e98348eda9883825728a5877
Nick Alcorso <nalcorso@gmail.com>
2022-05-10 10:22:38 +1000

    Separate interface to its own file

---
+++ src/fluid.inject/ITypeDescriptor.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2022, Nick Alcorso <nalcorso@gmail.com>
+
+namespace Fluid.Inject;
+
+//FIXME - Use a single monolithic interface until we are happy with the API. Ultimately we want to migrate to a state machine (of interfaces).
+public interface ITypeDescriptor
+{
+    Type? ConcreteType { get; }
+    object? Instance { get; }
+    Type? InterfaceType { get; }
+    Lifetime? Lifetime { get; }
+    string? Name { get; }
+
+    ITypeDescriptor As<TInterface>();
+    ITypeDescriptor AsSingleton();
+    ITypeDescriptor AsTransient();
+    ITypeDescriptor WithName(string name);
+    ITypeDescriptor WithInstance(object instance);
+}
--- src/fluid.inject/TypeDescriptor.cs
+++ src/fluid.inject/TypeDescriptor.cs
@@ -2,22 +2,6 @@
 
 namespace Fluid.Inject;
 
-//FIXME - Use a single monolithic interface until we are happy with the API. Ultimately we want to migrate to a state machine (of interfaces).
-public interface ITypeDescriptor
-{
-    Type? ConcreteType { get; }
-    object? Instance { get; }
-    Type? InterfaceType { get; }
-    Lifetime? Lifetime { get; }
-    string? Name { get; }
-
-    ITypeDescriptor As<TInterface>();
-    ITypeDescriptor AsSingleton();
-    ITypeDescriptor AsTransient();
-    ITypeDescriptor WithName(string name);
-    ITypeDescriptor WithInstance(object instance);
-}
-
 public class TypeDescriptor : ITypeDescriptor
 {
     public Type? ConcreteType { get; init; }
